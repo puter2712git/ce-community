053a6576273b0afb4f3eb4d8ded2bff9c35c045b
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-14 22:14:55 +0900

    fix: Update user columns

--- project/src/app/api/each-post/route.ts
+++ project/src/app/api/each-post/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
       content: true,
       author: {
         select: {
-          name: true,
+          nickname: true,
         },
       },
     },
