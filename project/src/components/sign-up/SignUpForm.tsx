b242381cec3ce0ff7bbd52d4e0ce47dac2432676
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-14 23:51:18 +0900

    fix: Change sign up form

--- project/src/components/sign-up/SignUpForm.tsx
+++ project/src/components/sign-up/SignUpForm.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { FieldErrors, useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
 
 interface ISignUpForm {
   nickname: string;
@@ -28,16 +31,29 @@ export default function SignUpForm() {
     getValues,
     setError,
     setFocus,
-  } = useForm<ISignUpForm>();
+  } = useForm<ISignUpForm>({
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      loginId: '',
+      password: '',
+      passwordCheck: '',
+      nickname: '',
+    },
+  });
+  const router = useRouter();
 
   async function onValid(data: ISignUpForm) {
     const res = await fetch('/api/auth/sign-up', {
       method: 'POST',
       body: JSON.stringify(data),
     });
-    console.log(res);
+    alert('회원가입 완료! 로그인 후 이용해 주세요!');
+    router.push('sign-in');
   }
-  function onInvalid(errors: FieldErrors<ISignUpForm>) {}
+  function onInvalid(errors: FieldErrors<ISignUpForm>) {
+    toast.warn('해결되지 않은 입력 오류가 존재합니다.');
+  }
 
   return (
     <form
@@ -55,8 +71,17 @@ export default function SignUpForm() {
           id="nickname"
           placeholder="닉네임: 2~8자"
           className="w-full px-[10px] py-[10px] border border-primary text-m"
-          {...register('nickname')}
+          {...register('nickname', {
+            required: '닉네임은 필수 항목입니다.',
+            minLength: {
+              value: 2,
+              message: '닉네임은 2~8자 이내로 입력해야 합니다.',
+            },
+          })}
         />
+        <span className="text-red-500 font-bold">
+          {errors.nickname?.message}
+        </span>
       </InputFieldWrapper>
 
       {/* 아이디 입력 */}
@@ -68,8 +93,21 @@ export default function SignUpForm() {
           id="loginId"
           placeholder="ID: 8~16자"
           className="w-full px-[10px] py-[10px] border border-primary text-m"
-          {...register('loginId')}
+          {...register('loginId', {
+            required: '아이디는 필수 항목입니다.',
+            minLength: {
+              value: 8,
+              message: '아이디는 8~16자 이내로 입력해야 합니다.',
+            },
+            maxLength: {
+              value: 16,
+              message: '아이디는 8~16자 이내로 입력해야 합니다.',
+            },
+          })}
         />
+        <span className="text-red-500 font-bold">
+          {errors.loginId?.message}
+        </span>
       </InputFieldWrapper>
 
       {/* 비밀번호 입력 */}
@@ -82,8 +120,21 @@ export default function SignUpForm() {
           type="password"
           placeholder="Password: 12~20자"
           className="w-full px-[10px] py-[10px] border border-primary text-m"
-          {...register('password')}
+          {...register('password', {
+            required: '비밀번호는 필수 항목입니다.',
+            minLength: {
+              value: 12,
+              message: '비밀번호는 12~16자 이내로 입력해야 합니다.',
+            },
+            maxLength: {
+              value: 16,
+              message: '비밀번호는 12~16자 이내로 입력해야 합니다.',
+            },
+          })}
         />
+        <span className="text-red-500 font-bold">
+          {errors.password?.message}
+        </span>
       </InputFieldWrapper>
 
       {/* 비밀번호 확인용 입력 */}
@@ -95,29 +146,39 @@ export default function SignUpForm() {
           id="password-check"
           type="password"
           className="w-full px-[10px] py-[10px] border border-primary text-m"
-          {...register('passwordCheck')}
+          {...register('passwordCheck', {
+            validate: (value) =>
+              value === getValues('password') ||
+              '비밀번호가 일치하지 않습니다.',
+          })}
         />
+        <span className="text-red-500 font-bold">
+          {errors.passwordCheck?.message}
+        </span>
       </InputFieldWrapper>
 
       {/* 이메일 입력 */}
       <InputFieldWrapper>
         <label htmlFor="email" className="font-bold text-m">
-          이메일
+          이메일 (선택사항)
         </label>
         <input
           id="email"
           type="email"
           placeholder="example@example.com"
           className="w-full px-[10px] py-[10px] border border-primary text-m"
-          {...register('email')}
+          {...register('email', {
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+              message: '잘못된 이메일 형식입니다.',
+            },
+          })}
         />
+        <span className="text-red-500 font-bold">{errors.email?.message}</span>
       </InputFieldWrapper>
 
       {/* 하단 버튼 */}
-      <div className="flex justify-between items-center w-full">
-        <button className="text-white text-m bg-gray-400 rounded-[10px] px-[8px] py-[4px]">
-          뒤로가기
-        </button>
+      <div className="flex justify-end items-center w-full">
         <button
           type="submit"
           className="text-white text-m bg-primary rounded-[10px] px-[8px] py-[4px]"
@@ -125,6 +186,13 @@ export default function SignUpForm() {
           회원가입
         </button>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        theme="light"
+      />
     </form>
   );
 }
