dc356106ef922344e1f99eefc505372226395741
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-16 00:22:38 +0900

    fix: Remove unused files

--- project/src/app/(pages)/write/page.tsx
+++ project/src/app/(pages)/write/page.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import WriteForm from '@/components/organisms/write-form';
 
-export default function PostPage() {
+export default function WritePage() {
   return (
     <div className="flex justify-center items-center w-full mt-20">
       <section className="w-1/3">
--- project/src/components/header/top/navigator/Navigator.tsx
+++ project/src/components/header/top/navigator/Navigator.tsx
@@ -2,7 +2,7 @@ import NavLink, { INavLink } from './NavLink';
 
 const links: (INavLink & { id: number })[] = [
   { id: 1, href: '/board', children: '게시판' },
-  { id: 2, href: '/project', children: '프로젝트' },
+  { id: 2, href: '/', children: '프로젝트' },
 ];
 
 export default function Navigator() {
