c92a773738ee782af67958ae4ca1c443109a0086
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-16 00:12:35 +0900

    fix: Remove unused props

--- project/src/app/(pages)/board/page.tsx
+++ project/src/app/(pages)/board/page.tsx
@@ -1,6 +1,6 @@
 import Board from '@/components/board/Board';
 
-export default function BoardPage(props: any) {
+export default function BoardPage() {
   return (
     <div className="flex justify-center w-full">
       <article className="w-1/2 max-w-[1200px]">
