031d6b8e793d38c120506cc9da5744c96b96b7e4
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-23 21:42:20 +0900

    test: Delete force dynamic

--- project/src/app/api/board/route.ts
+++ project/src/app/api/board/route.ts
@@ -1,16 +1,7 @@
 import prisma from '@/lib/db';
-import { verifyJwt } from '@/lib/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
 export async function GET(req: NextRequest) {
-  //   const accessToken = req.headers.get('authorization');
-  //   if (!accessToken || !verifyJwt(accessToken)) {
-  //     return new Response(JSON.stringify({ error: 'No Authorization' }), {
-  //       status: 401,
-  //     });
-  //   }
-
   const datas = await prisma.post.findMany({
     select: {
       id: true,
