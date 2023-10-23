806f48119a6bf49493c42f8dc4cb7ba953bb57a1
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-23 21:58:21 +0900

    fix: Add force-dynamic config in board api endpoint

--- project/src/app/api/board/route.ts
+++ project/src/app/api/board/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 
+const dynamic = 'force-dynamic';
 export async function GET(req: NextRequest) {
   const datas = await prisma.post.findMany({
     select: {
