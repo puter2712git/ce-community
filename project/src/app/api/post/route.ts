221f33b172a75894bb412e17265e88df93965fac
Dongjin Kuk <puter2712goo@gmail.com>
2023-10-26 17:17:33 +0900

    fix: SSR for each post page

--- project/src/app/api/post/route.ts
+++ project/src/app/api/post/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const post = await prisma.post.findUnique({
